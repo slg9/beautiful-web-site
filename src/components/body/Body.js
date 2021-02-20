7b3bb409344aacdd9146fcd289e4debea7914f02
Sebastien legros <slegros9@gmail.com>
2021-02-20 11:47:51 +0400

    my first animations

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import "./body.css"
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import Logo from "../../assets/programming.svg";
+import "./body.css";
 
 function Body() {
-    return (
-        <div className="body">
-            body
-        </div>
-    )
+  const fadein = useSpring({ opacity: 1, from: { opacity: 0 } });
+  const growup = useSpring({ width: "100px", from: { width: "0px" } });
+  const progress = useSpring({ x: 100, from: { x: 0 } });
+  return (
+    <div className="body">
+      <animated.h1 style={fadein}>voici ma premiere animation</animated.h1>
+      <animated.img src={Logo} style={growup} />
+      <animated.span>{progress.x}</animated.span>
+    </div>
+  );
 }
 
-export default Body
+export default Body;
